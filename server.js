f4a6124f9d5986955ce90e8330ea00c912813947
Sheel Patel <sheel@letswhirl.com>
2023-04-26 16:11:17 -0400

    decide which to parse as

--- server.js
+++ server.js
@@ -19,20 +19,20 @@ import('node-rpsls').then((module) => {
         'spock'
     ]
 
-    const rpsRouter = express.Router({ mergeParams: true})
+    const rpsRouter = express.Router({ mergeParams: true })
         .get('/', (req, res) => {
             res.status(200).send(rps())
         })
         .get('/play/', (req, res) => {
             console.log(req.body)
             console.log(req.headers)
-                let { shot } = req.body || {}
-                shot = shot?.toLowerCase?.() || ''
-                // if (!rpsAcceptedShots.includes(shot)) {
-                //     res.status(400).send("400 BAD REQUEST")
-                //     return;
-                // }
-                res.status(200).send(rps(shot))
+            let { shot } = req.body || {}
+            shot = shot?.toLowerCase?.() || ''
+            // if (!rpsAcceptedShots.includes(shot)) {
+            //     res.status(400).send("400 BAD REQUEST")
+            //     return;
+            // }
+            res.status(200).send(rps(shot))
         })
         .get("/play/:shot", (req, res) => {
             let { shot } = req.params
@@ -43,18 +43,18 @@ import('node-rpsls').then((module) => {
             // }
             res.status(200).send(rps(shot))
         })
-    const rpslsRouter = express.Router({ mergeParams: true})
+    const rpslsRouter = express.Router({ mergeParams: true })
         .get('/', (req, res) => {
             res.status(200).send(rps())
         })
         .get('/play/', (req, res) => {
-                let { shot } = req.body || {}
-                shot = shot?.toLowerCase?.() || ''
-                // if (!rpslsAcceptedShots.includes(shot)) {
-                //     res.status(400).send("400 BAD REQUEST, not an acceptable shot")
-                //     return;
-                // }
-                res.status(200).send(rpsls(shot))
+            let { shot } = req.body || {}
+            shot = shot?.toLowerCase?.() || ''
+            // if (!rpslsAcceptedShots.includes(shot)) {
+            //     res.status(400).send("400 BAD REQUEST, not an acceptable shot")
+            //     return;
+            // }bean
+            res.status(200).send(rpsls(shot))
         })
         .get("/play/:shot", (req, res) => {
             let { shot } = req.params
@@ -66,7 +66,7 @@ import('node-rpsls').then((module) => {
             res.status(200).send(rpsls(shot))
         })
 
-    const appRouter = express.Router({ mergeParams: true})
+    const appRouter = express.Router({ mergeParams: true })
         .get("/", (req, res) => {
             res.status(200).send('200 OK')
         })
@@ -78,13 +78,32 @@ import('node-rpsls').then((module) => {
 
     const app = express()
 
-    app.use(express.urlencoded({ extended: false }));
+    app.use(express.urlencoded({ extended: true }));
     app.use(express.json());
+
+    app.use((req, res, next) => {
+        // If the Content-Type header is missing, try parsing the request body as URL-encoded form data first
+        if (!req.headers['content-type']) {
+            try {
+                express.urlencoded({ extended: true })(req, res, next);
+            } catch (error) {
+                // If parsing as URL-encoded form data fails, try parsing as JSON next
+                if (error instanceof SyntaxError) {
+                    express.json()(req, res, next);
+                } else {
+                    next(error);
+                }
+            }
+        } else {
+            next();
+        }
+    });
+
     app.use("/app", appRouter)
-    
+
     app.use((req, res, next) => {
         res.status(404).send('404 NOT FOUND');
-      });
+    });
 
     app.listen(PORT)
 
