cefdf90ee0d1d1c953d6d9a19ef5498083cc0322
Sheel Patel <sheel@letswhirl.com>
2023-04-26 16:34:27 -0400

    fixed middleware

--- server.js
+++ server.js
@@ -1,10 +1,39 @@
 // require = require("esm")(module);
+
+const { parse } = require('path')
+
 // const  = require('node-rpsls').default;
 import('node-rpsls').then((module) => {
     const { rps, rpsls } = module
     const minimist = require('minimist')
     const express = require('express')
     const querystring = require('querystring')
+
+    function parseBody(req, res, next) {
+        let data = '';
+      
+        req.on('data', chunk => {
+          data += chunk;
+        });
+      
+        req.on('end', () => {
+          if (data) {
+            try {
+              // Try to parse as JSON
+              req.body = JSON.parse(data);
+              next();
+            } catch (e) {
+              // If JSON parsing fails, parse as URL-encoded
+              req.body = require('querystring').parse(data);
+              next();
+            }
+          } else {
+            req.body = {};
+            next();
+          }
+        });
+      }
+      
 
     const rpsAcceptedShots = [
         'rock',
@@ -79,40 +108,62 @@ import('node-rpsls').then((module) => {
 
     const app = express()
 
-    app.use(express.urlencoded({ extended: true }));
-    app.use(express.json());
-
-    
-app.use((req, res, next) => {
-    if (!req.headers['content-type']) {
-      // If the Content-Type header is missing, manually parse the request body
-      let body = '';
-      req.on('data', (chunk) => {
-        body += chunk;
-      });
-      req.on('end', () => {
-         console.log("B: ", body)
-        try {
-          // Try parsing the body as URL-encoded form data
-          const parsedBody = querystring.parse(body);
-          req.body = parsedBody;
-          next();
-        } catch (error) {
-          try {
-            // If parsing as URL-encoded form data fails, try parsing as JSON
-            const parsedBody = JSON.parse(body);
-            req.body = parsedBody;
+    app.use((req, res, next) => {
+        let data = '';
+      
+        req.on('data', chunk => {
+          data += chunk;
+        });
+      
+        req.on('end', () => {
+          if (data) {
+            try {
+              // Try to parse as JSON
+              req.body = JSON.parse(data);
+              next();
+            } catch (e) {
+              // If JSON parsing fails, parse as URL-encoded
+              req.body = require('querystring').parse(data);
+              next();
+            }
+          } else {
+            req.body = {};
             next();
-          } catch (error) {
-            // If parsing as JSON also fails, return a 400 Bad Request error
-            res.status(400).send('Invalid Request Body');
           }
-        }
-      });
-    } else {
-      next();
-    }
-  });
+        });
+      })
+    // app.use(express.urlencoded({ extended: true }));
+    // app.use(express.json());
+    
+// app.use((req, res, next) => {
+//     if (!req.headers['content-type']) {
+//       // If the Content-Type header is missing, manually parse the request body
+//       let body = '';
+//       req.on('data', (chunk) => {
+//         body += chunk;
+//       });
+//       req.on('end', () => {
+//          console.log("B: ", body)
+//         try {
+//           const parsedBody = querystring.parse(body);
+//           req.body = parsedBody;
+//           next();
+//         } catch (error) {
+//           try {
+//             // req.headers['content-type'] = 'application/json';
+//             // express.json({ type: '*/*' })(req, res, next);
+//             const parsedBody = JSON.parse(body);
+//             req.body = parsedBody;
+//             next();
+//           } catch (error) {
+//             res.status(400).send();
+//           }
+//         }
+//       });
+//     } else {
+//       next();
+//     }
+//   });
 
     app.use("/app", appRouter)
 
